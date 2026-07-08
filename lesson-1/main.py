9a9ad0317edc29a82503e7cb794672044efb81b1
Oscar <oguzman@basecampcodingacademy.org>
2026-07-07 20:05:57 -0500

    Unit 2

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -7,6 +7,14 @@ class Cat:
     is_hungry: bool
 
 
+
+def get_choice():
+    while True:
+        choice = input("Feed [Kit], Feed [Kat], [quit]? ")
+        if choice == "Kit" or choice == "Kat" or choice == "quit":
+            return choice
+        print("Please provide a valid cat name or quit.")
+
 def cat_sound(cat: Cat) -> str:
     if cat.is_hungry:
         return "hiss"
@@ -14,5 +22,28 @@ def cat_sound(cat: Cat) -> str:
         return "meow"
 
 
-def feed_cat(cat: Cat) -> None:
+def feed_cat(cat: Cat):
     cat.is_hungry = False
+
+
+def main():
+    kit = Cat("Kit", True)
+    kat = Cat("Kat", True)
+
+    while True:
+        print(f"{kit.name} says {cat_sound(kit)}")
+        print(f"{kat.name} says {cat_sound(kat)}")
+
+        choice = get_choice()
+        if choice == "quit":
+            break
+        elif choice == "Kit":
+            print("Feeding Kit")
+            feed_cat(kit)
+        elif choice == "Kat":
+            print("Feeding Kat")
+            feed_cat(kat)
+
+
+if __name__ == "__main__":
+    main()
